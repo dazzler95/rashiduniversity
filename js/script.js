e6cca4cea7f6dc6754b10eaca07a1eb354e274fc
socialbitdigital <socialbitdigital@gmail.com>
2020-08-29 01:12:36 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(".slides").slick({
+  autoplay: true,
+  autoplaySpeed: 3500,
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  arrows: false,
+  fade: true,
+  cssEase: "ease-out",
+  speed: 500,
+});
+
+var login = document.querySelector("#login");
